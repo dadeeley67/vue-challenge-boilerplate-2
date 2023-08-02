1b876079defe01d577116badfc285633a139530f
Dave <dadeeley67@proton.me>
2023-08-02 08:58:38 -0400

    Added local storage to persit changes on refresh

--- src/stores/athlete.js
+++ src/stores/athlete.js
@@ -1,9 +1,11 @@
 import { defineStore } from "pinia";
+import { useLocalStorage } from "@vueuse/core";
+
 import AthleteData from "@/assets/data.json";
 
 export const useAthleteStore = defineStore("useAthleteStore", {
   state: () => ({
-    athlete: AthleteData.data[0],
+    athlete: useLocalStorage("athlete", AthleteData.data[0]),
   }),
   actions: {
     setAthleteName(name) {
